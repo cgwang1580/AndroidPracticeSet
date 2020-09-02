686a6fa379874ad5c83b054a601fcda8d331f2b1
cgwang1580 <cgwang1580@gmail.com>
2020-09-02 14:27:08 +0800

    增加AsyncTask加载Bitmap

--- AlbumTest/app/src/main/java/com/example/albumtest/AlbumActivity.java
+++ AlbumTest/app/src/main/java/com/example/albumtest/AlbumActivity.java
@@ -4,6 +4,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.FileProvider;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
@@ -12,24 +13,33 @@ import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.media.MediaScannerConnection;
 import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Environment;
+import android.os.ParcelFileDescriptor;
 import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.utils.MyLog;
 
 import java.io.File;
+import java.io.FileDescriptor;
+import java.io.IOException;
 
 public class AlbumActivity extends AppCompatActivity {
 
     private final String TAG = this.getClass().getName();
 
-    private final int REQUEST_CODE_OPEN_ALBUM = 0;
+    private final int REQUEST_CODE_OPEN_CAMERA = 0x100;
+    private final int REQUEST_CODE_OPEN_DOC = 0x101;
+    private final int REQUEST_CODE_OPEN_ALBUM = 0x102;
     private ImageView mImageView = null;
-    private Button mBtnOpenAlbum =null;
+    private Button mBtnOpenAlbum = null;
+    private TextView mTextViewInfo = null;
     private Bitmap mBitmap = null;
 
     @Override
@@ -41,7 +51,8 @@ public class AlbumActivity extends AppCompatActivity {
 
     private void initUI () {
         mImageView = this.findViewById(R.id.image_view);
-        mBtnOpenAlbum = this.findViewById(R.id.btn_open);
+        mBtnOpenAlbum = this.findViewById(R.id.btn_open_album);
+        mTextViewInfo = this.findViewById(R.id.text_view_info);
         mBtnOpenAlbum.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,8 +65,21 @@ public class AlbumActivity extends AppCompatActivity {
                 saveImageToGallery();
             }
         });
-    }
 
+        findViewById(R.id.btn_camera).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSystemCamera();
+            }
+        });
+
+        findViewById(R.id.btn_document).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openSystemDocument();
+            }
+        });
+    }
     private void openSystemAlbum () {
         MyLog.d(TAG, "openSystemAlbum");
         Intent albumIntent = new Intent(Intent.ACTION_PICK);
@@ -68,6 +92,12 @@ public class AlbumActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (RESULT_OK != resultCode) return;
         switch (requestCode) {
+            case REQUEST_CODE_OPEN_CAMERA:
+                saveCaptureImage (data);
+                break;
+            case REQUEST_CODE_OPEN_DOC:
+                selectDocFile (data);
+                break;
             case REQUEST_CODE_OPEN_ALBUM:
                 selectPicture(data);
                 break;
@@ -113,7 +143,8 @@ public class AlbumActivity extends AppCompatActivity {
         if (mBitmap != null) {
             mImageView.setBackground(new BitmapDrawable(getResources(), mBitmap));
             //String pathImg = MediaStore.Images.Media.insertImage(getContentResolver(), bitmap, "", "");
-            GalleryFileSaver.saveBitmapToGallery(this, "test_avatar2.jpeg", mBitmap);
+            String path = "test_" + System.currentTimeMillis()/1000 + ".jpeg";
+            GalleryFileSaver.saveBitmapToGallery(this, path, mBitmap);
             //GalleryFileSaver.saveToSystemGallery(this, mBitmap);
         }
     }
@@ -138,5 +169,104 @@ public class AlbumActivity extends AppCompatActivity {
             c.close();
         }
         return filePath;
+    }
+
+    private void openSystemCamera() {
+        String imagePath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM) + "/Camera/" + System.currentTimeMillis()+".jpeg";
+        File filePath = new File(imagePath);
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //judge sdk version
+        if (Build.VERSION.SDK_INT > Build.VERSION_CODES.N) {
+            //如果在Android7.0以上,使用FileProvider获取Uri
+            cameraIntent.setFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+            Uri contentUri = FileProvider.getUriForFile(this, "com.example.albumtest", filePath);
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, contentUri);
+        } else {
+            //否则使用Uri.fromFile(file)方法获取Uri
+            cameraIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(filePath));
+        }
+        cameraIntent.putExtra("path", imagePath);
+        startActivityForResult(cameraIntent, REQUEST_CODE_OPEN_CAMERA);
+    }
+
+    private void saveCaptureImage (Intent intent) {
+        if (intent != null) {
+            String path = intent.getStringExtra("path");
+            Bitmap bitmap = BitmapFactory.decodeFile(path);
+            setImageViewBackground (mImageView, bitmap);
+        }
+    }
+
+    private void openSystemDocument () {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_CODE_OPEN_DOC);
+    }
+
+    private void selectDocFile (Intent intent) {
+        Uri uri = null;
+        if (intent != null) {
+            uri = intent.getData();
+            MyLog.d(TAG, "selectDocFile " + uri.toString());
+        }
+        /*Bitmap bitmap = getBitmapFromUri(uri);
+        setImageViewBackground (mImageView, bitmap);*/
+        ImageFileTask imageFileTask = new ImageFileTask();
+        imageFileTask.execute(uri);
+    }
+
+    private Bitmap getBitmapFromUri (Uri uri) {
+        MyLog.d(TAG, "getBitmapFromUri uri = " + uri.toString());
+        Bitmap bitmap = null;
+        try {
+            ParcelFileDescriptor parcelFileDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+            FileDescriptor fileDescriptor = parcelFileDescriptor.getFileDescriptor();
+            bitmap = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+            parcelFileDescriptor.close();
+        } catch (IOException ioe) {
+            ioe.printStackTrace();
+        }
+        return bitmap;
+    }
+
+    private void setImageViewBackground (ImageView imageView, Bitmap bitmap) {
+        if (imageView != null && bitmap != null) {
+            imageView.setBackground(new BitmapDrawable(getResources(), bitmap));
+        } else {
+            MyLog.e(TAG, "setImageViewBackground error");
+        }
+    }
+
+    private class ImageFileTask extends AsyncTask<Uri, Integer, Bitmap> {
+
+        @SuppressLint("SetTextI18n")
+        @Override
+        protected void onPreExecute() {
+            MyLog.d(TAG, "onPreExecute");
+            mTextViewInfo.setText("loading");
+        }
+
+        @Override
+        protected Bitmap doInBackground(Uri... uris) {
+            return getBitmapFromUri (uris[0]);
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(Bitmap bitmap) {
+            super.onPostExecute(bitmap);
+            setImageViewBackground(mImageView, bitmap);
+            mTextViewInfo.setText("done");
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
     }
 }
